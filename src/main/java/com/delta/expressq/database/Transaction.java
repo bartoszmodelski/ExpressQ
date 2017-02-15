7c8f81f4003d96115ed6c20ebca4221516268357
Matthew Brighty <MBrighty@users.noreply.github.com>
2017-02-15 14:33:59 +0000

    Edited getTransaction and transaction classes to reflect changes to DB schema

--- src/main/java/com/delta/expressq/database/Transaction.java
+++ src/main/java/com/delta/expressq/database/Transaction.java
@@ -4,16 +4,16 @@ public class Transaction{
     public int transactionID;
     public int userID;
     public int venueID;
-    public int listID;
     public float total;
     public String date;
+    public boolean issued;
 
-    Transaction(int tID, int uID, int vID, int lID, float p, String d){
+    Transaction(int tID, int uID, int vID, float p, String d, boolean i){
         this.transactionID = tID;
         this.userID = uID;
         this.venueID = vID;
-        this.listID = lID;
         this.total = p;
         this.date = d;
+        this.issued = i;
     }
 }
