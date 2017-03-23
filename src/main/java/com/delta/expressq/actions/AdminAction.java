3078a0449ac039aa71c5287535a432619c590709
Grant Christie <grantchristie0@gmail.com>
2017-03-23 15:28:16 +0000

    javadocs for venueaction

--- src/main/java/com/delta/expressq/actions/AdminAction.java
+++ src/main/java/com/delta/expressq/actions/AdminAction.java
@@ -19,7 +19,7 @@ public class AdminAction extends ActionSupportWithSession implements ServletRequ
 	/**
 	 * Calls the DisplayUsers method within ConnectionManager to retrieve the users from the database and sets the user object to "disp"
 	 * so display.jsp can access the data. 
-	 * @return SUCCESS if an admin user is trying to access the page, otherwise return "denied".
+	 * @return SUCCESS if an admin user is trying to access the page, otherwise return "permission_error".
 	 */
 	public String Display(){
 		if(isLoggedIn()){
@@ -33,18 +33,18 @@ public class AdminAction extends ActionSupportWithSession implements ServletRequ
 				request.setAttribute("disp", users);
 				return SUCCESS;
 			}else{
-				return "denied";
+				return "permission_error";
 			}
 		}else{
-			return "denied";
+			return "permission_error";
 		}
 	}
 
 	/**
 	 * This method gets the values from display.jsp that have been selected for deletion and passes them into the DeleteUsers method
 	 * in ConnectionManager.This method will then carry out the required processes to delete the user(s). 
-	 * If users have been selected for deletion the program will return an error
-	 * @return ERROR if no user has been selected for deletion. SUCCESS if a user has been selected. "denied" if the user trying to access the method is not an admin.
+	 * If no users have been selected for deletion the function will return an error
+	 * @return ERROR if no user has been selected for deletion. SUCCESS if a user has been selected. "permission_error" if the user trying to access the method is not an admin.
 	 */
 	public String Delete(){
 		if (isLoggedIn()){
@@ -62,17 +62,17 @@ public class AdminAction extends ActionSupportWithSession implements ServletRequ
 					return SUCCESS;
 				}
 			}else{
-				return "denied";
+				return "permission_error";
 			}
 		}else{
-			return "denied";
+			return "permission_error";
 		}
 	}
 	
 	/**
 	 * Retrieves the selectedID from the display.jsp file and calls methods held within connection manager to get the 
 	 * userDetails required to populate the fields in the edituser.jsp page.
-	 * @return SUCCESS if the UserID selected exists. ERROR if it does not. "denied" if the user trying to access the method is not an admin.
+	 * @return SUCCESS if the UserID selected exists. ERROR if it does not. "permission_error" if the user trying to access the method is not an admin.
 	 */
 	public String Edit(){
 		if(isLoggedIn()){
@@ -90,15 +90,15 @@ public class AdminAction extends ActionSupportWithSession implements ServletRequ
 					return "db_error";
 				}
 			}else {
-				return "denied";
+				return "permission_error";
 			}
 		}else
-			return "denied";
+			return "permission_error";
 	}
 	
 	/**
 	 * This calls a method within ConnectionManager that updates the database with the changes that have been made by the user.
-	 * @return SUCCESS if an admin user is trying to access the page, otherwise return "denied".
+	 * @return SUCCESS if an admin user is trying to access the page, otherwise return "permission_error".
 	 */
 	public String UpdateUserDetails(){
 		if(isLoggedIn()){
@@ -112,16 +112,16 @@ public class AdminAction extends ActionSupportWithSession implements ServletRequ
 				}
 				return SUCCESS;
 			}else{
-				return "denied";
+				return "permission_error";
 			}
 		}else{
-			return "denied";
+			return "permission_error";
 		}
 	}
 	
 	/**
 	 * When the user selects to insert a new user this method returns SUCCESS which is used to redirect the user to the page used to create a new user.
-	 * @return SUCCESS if an admin user is trying to access the page, otherwise return "denied".
+	 * @return SUCCESS if an admin user is trying to access the page, otherwise return "permission_error".
 	 */
 	public String Insert(){
 		if(isLoggedIn()){
@@ -129,10 +129,10 @@ public class AdminAction extends ActionSupportWithSession implements ServletRequ
 			if (adminuser.getType() == 1){
 				return SUCCESS;
 			}else{
-				return "denied";
+				return "permission_error";
 			}
 		}else{
-			return "denied";
+			return "permission_error";
 		}
 	}
 	
--- src/main/java/com/delta/expressq/actions/VenueAction.java
+++ src/main/java/com/delta/expressq/actions/VenueAction.java
@@ -21,6 +21,10 @@ public class VenueAction extends ActionSupportWithSession implements ServletRequ
 	public String selectedSectionID, Name, NewName, sectionID, selectedItemID;
 	private int preparationtime, stock, price, itemID;
 	
+	/**
+	 * Displays a venue's list of sections given the that is logged in is a venue user. Otherwise the no permission page is displayed.
+	 * @return Success if user passes login check, otherwise permission_error.
+	 */
 	public String execute(){
 		if (isLoggedIn()){
 			UserNew user = getUserObject();
@@ -39,19 +43,14 @@ public class VenueAction extends ActionSupportWithSession implements ServletRequ
 		}
 	}
 	
+	/**
+	 * Displays the page that allows the venue user to add a new section to their menu.
+	 * @return Success if user passes login check otherwise permission_error.
+	 */
 	public String NewSection(){
-		return SUCCESS;
-	}
-	
-	public String InsertSection(){
-		if (isLoggedIn()){
+		if(isLoggedIn()){
 			UserNew user = getUserObject();
 			if (user.getType() == 2){
-				try {
-					ConnectionManager.InsertSection(ConnectionManager.getVenueID(user.getUserID()), description);
-				} catch (ConnectionManagerException e) {
-					e.printStackTrace();
-				}
 				return SUCCESS;
 			}else{
 				return "permission_error";
@@ -61,6 +60,34 @@ public class VenueAction extends ActionSupportWithSession implements ServletRequ
 		}
 	}
 	
+	/**
+	 * Takes value description from jsp file and passes it along with the venueid of the user to ConnectionManager.getVenueID(). This adds the section to the database.
+	 * @return SUCCESS if operation is successfully otherwise return db_error. If user login is not valid return permission_error.
+	 */
+	public String InsertSection(){
+		if (isLoggedIn()){
+			UserNew user = getUserObject();
+			if (user.getType() == 2){
+				try {
+					ConnectionManager.InsertSection(ConnectionManager.getVenueID(user.getUserID()), description);
+				} catch (ConnectionManagerException e) {
+					e.printStackTrace();				
+					return "db_error";}
+				return SUCCESS;
+			}else{
+				return "permission_error";
+			}
+		}else{
+			return "permission_error";
+		}
+	}
+	
+	/**
+	 * This method gets the values from the jsp that have been selected for deletion and passes them into the DeleteSections method
+	 * in ConnectionManager.This method will then carry out the required processes to delete the section(s). 
+	 * If no sections have been selected for deletion the function will return an error
+	 * @return ERROR if no item has been selected for deletion. SUCCESS if a section has been selected. "permission_error" if the user trying to access the method is not the valid venue owner.
+	 */
 	public String DeleteSection(){
 		if (isLoggedIn()){
 			UserNew user = getUserObject();
@@ -85,6 +112,11 @@ public class VenueAction extends ActionSupportWithSession implements ServletRequ
 		}
 	}
 	
+	/**
+	 * Gets the ID of the section that has been selected to edit by the user and populates the jsp file with it's current properties so the user can make changes.
+	 * "Name" is used to pass the name of the section into the jsp.
+	 * @return SUCCESS if the fetch from the database was successful. db_error if it was not. permission_error if the user currently logged in is not valid.
+	 */
 	public String EditSection(){
 		if(isLoggedIn()){
 			UserNew user = getUserObject();
@@ -103,6 +135,10 @@ public class VenueAction extends ActionSupportWithSession implements ServletRequ
 			return "permission_error";
 	}
 	
+	/**
+	 * Update section given the changes the user made in the page generated in the EditSection() method.
+	 * @return SUCCESS if the update was a success. db_error if it was a fail. permission_error if the user is not valid.
+	 */
 	public String UpdateSection(){
 		if(isLoggedIn()){
 			UserNew user = getUserObject();
@@ -122,6 +158,11 @@ public class VenueAction extends ActionSupportWithSession implements ServletRequ
 		}
 	}
 	
+	/**
+	 * Displays the items belonging to the section that the user has selected. Passes the sectionID into ConnectionManager.getItemsBySection() which returns a map contained all the items.
+	 * JSP iterates through the map to display the items.
+	 * @return SUCCESS if operation is successful. permission_error if user is invalid.
+	 */
 	public String DisplayItems(){
 		if(isLoggedIn()){
 			UserNew user = getUserObject();
@@ -141,6 +182,10 @@ public class VenueAction extends ActionSupportWithSession implements ServletRequ
 		}
 	}
 	
+	/**
+	 * Displays the page that allows the venue user to add a new item to their section.
+	 * @return Success if user passes login check otherwise permission_error.
+	 */
 	public String NewItem(){
 		if (isLoggedIn()){
 			UserNew user = getUserObject();
@@ -154,6 +199,10 @@ public class VenueAction extends ActionSupportWithSession implements ServletRequ
 		}
 	}
 	
+	/**
+	 * Insert a new item to the database with the values the user has filled into the fields in the jsp file. 
+	 * @return SUCCESS if the operation was a success. db_error if it failed. permission_error if the user is not valid.
+	 */
 	public String InsertItem(){
 		if (isLoggedIn()){
 			UserNew user = getUserObject();
@@ -162,6 +211,7 @@ public class VenueAction extends ActionSupportWithSession implements ServletRequ
 					ConnectionManager.InsertItem(sectionID, price, itemname, itemdescription, stock, allergens, preparationtime);
 				} catch (ConnectionManagerException e) {
 					e.printStackTrace();
+					return "db_error";
 				}
 				return SUCCESS;
 			}else{
@@ -172,6 +222,12 @@ public class VenueAction extends ActionSupportWithSession implements ServletRequ
 		}
 	}
 	
+	/**
+	 * This method gets the values from the jsp that have been selected for deletion and passes them into the DeleteItems method
+	 * in ConnectionManager.This method will then carry out the required processes to delete the item(s). 
+	 * If no items have been selected for deletion the function will return an error
+	 * @return ERROR if no item has been selected for deletion. SUCCESS if a item has been selected. "permission_error" if the user trying to access the method is not the valid venue owner.
+	 */
 	public String DeleteItem(){
 		if (isLoggedIn()){
 			UserNew user = getUserObject();
@@ -195,6 +251,11 @@ public class VenueAction extends ActionSupportWithSession implements ServletRequ
 		}
 	}
 	
+	/**
+	 * Gets the ID of the item that has been selected to edit by the user and populates the jsp file with it's current properties so the user can make changes.
+	 * "itemDetails" is used to pass the details of the item into the jsp.
+	 * @return SUCCESS if the fetch from the database was successful. db_error if it was not. permission_error if the user currently logged in is not valid.
+	 */	
 	public String EditItem(){
 		if(isLoggedIn()){
 			UserNew user = getUserObject();
@@ -216,6 +277,10 @@ public class VenueAction extends ActionSupportWithSession implements ServletRequ
 			return "permission_error";
 	}
 	
+	/**
+	 * Update item given the changes the user made in the page generated in the EditItem() method.
+	 * @return SUCCESS if the update was a success. db_error if it was a fail. permission_error if the user is not valid.
+	 */
 	public String UpdateItem(){
 		if(isLoggedIn()){
 			UserNew user = getUserObject();
@@ -270,10 +335,6 @@ public class VenueAction extends ActionSupportWithSession implements ServletRequ
 	public String getSectionSelectedID(){
 		return selectedSectionID;
 	}
-	/*
-	public void setSectionSelectedID(String selectedSectionID){
-		this.selectedSectionID = selectedSectionID;
-	}*/
 	
 	public String getName(){
 		return Name;
